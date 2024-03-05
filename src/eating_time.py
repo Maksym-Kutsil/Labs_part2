a08f37c16573b9aa89db1f643e6ad51b48f1b22e
Maksym-Kutsil <kucilmaksim@gmail.com>
2024-03-05 11:24:31 +0300

    added sort function

--- src/eating_time.py
+++ src/eating_time.py
@@ -1,27 +1,24 @@
 def can_eat(piles, hours, k):
-    sorted(piles)
+    piles.sort()
     eating_hours = 0
     for pile in piles:
         eating_hours += (pile + k - 1) // k
     return eating_hours <= hours
 
-
 def min_eating_speed(piles, h):
-    sorted(piles)
+    piles.sort()
     if h < len(piles):
         return None
-
     left = 1
     right = max(piles)
-
     while left <= right:
         mid = (left + right) // 2
         if can_eat(piles, h, mid):
             right = mid - 1
         else:
             left = mid + 1
-
     return left
+
 
 
 print(min_eating_speed([4, 10, 3, 8, 6], 7))
