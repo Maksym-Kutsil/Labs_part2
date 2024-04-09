21510df7285be0e6a384aed5d962cb934f942cd7
Maksym-Kutsil <kucilmaksim@gmail.com>
2024-04-09 08:35:59 +0300

    changed

--- src/maze.py
+++ src/maze.py
@@ -1,7 +1,6 @@
 def shotres_way(start_point, finish_point, size_of_labyrinth, labyrinth):
     queue = [start_point]
     dist_to = {start_point: 0}
-    prevex_vertex = {start_point: None}
     visited = set()
     while queue:
         vertex = queue.pop(0)
@@ -29,18 +28,14 @@ def read_input(input_file):
         start_str = lines[0].strip()
         finish_str = lines[1].strip()
         size_str = lines[2].strip()
-        lab = []
+        maze = []
         for line in lines[3:]:
             row = [int(x) for x in line.strip().split(',') if x]
-            lab.append(row)
+            maze.append(row)
     start = tuple(map(int, start_str.split(',')))
     finish = tuple(map(int, finish_str.split(',')))
     size = tuple(map(int, size_str.split(',')))
-    return start, finish, size, lab
-
-
-
-
+    return start, finish, size, maze
 
 def write_output(output_file, result):
     with open(output_file, 'w') as f:
--- tests/test_maze.py
+++ tests/test_maze.py
@@ -1,7 +1,7 @@
 import unittest
 from src.maze import *
 
-class TestMaze(unittest.TestCase):
+class EmptyMaze(unittest.TestCase):
 
     def test_single_path_maze(self):
         input_file = 'C:/Users/User/Desktop/Lab 5/resources/test_input.txt'
@@ -11,7 +11,26 @@ class TestMaze(unittest.TestCase):
 
         result = shotres_way(start, finish, size, labyrinth)
 
-        expected_result = 10
+        expected_result = None
+
+        write_output(output_file, result)
+
+        self.assertEqual(result, expected_result)
+
+if __name__ == "__main__":
+    unittest.main()
+
+class TestMaze(unittest.TestCase):
+
+    def test_single_path_maze(self):
+        input_file = 'C:/Users/User/Desktop/Lab 5/resources/test2_input.txt'
+        output_file = 'C:/Users/User/Desktop/Lab 5/resources/test2_output.txt'
+
+        start, finish, size, maze = read_input(input_file)
+
+        result = shotres_way(start, finish, size, maze)
+
+        expected_result = 9
 
         write_output(output_file, result)
 
