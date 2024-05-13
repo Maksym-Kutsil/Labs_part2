2c717d1dd8000ffb7f67c7bf8711da946d03528c
Maksym-Kutsil <kucilmaksim@gmail.com>
2024-05-14 00:27:42 +0300

    changed ways

--- tests/test_max_flow.py
+++ tests/test_max_flow.py
@@ -4,9 +4,9 @@ from src.max_flow import max_flow
 
 class TestFlowersMaxFlow(unittest.TestCase):
     def test_farms_stores_graph(self):
-        result = max_flow("roads.csv")
+        result = max_flow("../resourses/roads.csv")
         self.assertEqual(result, 15)
 
     def test_empty_input(self):
-        result = max_flow("empty_roads.csv")
+        result = max_flow("../resourses/empty_roads.csv")
         self.assertEqual(result, None)
